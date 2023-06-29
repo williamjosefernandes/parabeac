691b889c84c72ea2b53bfc71fcf9b935be5c4a75
Parabeac-Cloud <admin@parabeac.com>
2023-06-29 14:54:48 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/explorador_text_styles.g.dart
@@ -0,0 +1,43 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ExploradorTextStyles {
+/** Previous style:
+H2/bold **/
+static const TextStyle h2Bold = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/** Previous style:
+body/large/bold **/
+static const TextStyle bodyLargeBold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/** Previous style:
+body/large/semibold **/
+static const TextStyle bodyLargeSemibold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'Urbanist',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
